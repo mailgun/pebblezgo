1d67a8f379e3d5e5aa589be02a5898480f51c8bd
Jason E. Aten <j.e.aten@gmail.com>
2014-09-22 14:04:52 -0700

    quiet :aot warining in project.clj

--- clj/project.clj
+++ clj/project.clj
@@ -8,6 +8,14 @@
                 ]
   :plugins [[lein-protobuf "0.2.0"]
  ]
+  :aot [clojure-protobuf-demo.core]
   :main clojure-protobuf-demo.core
   :auto-clean false
+
+ :profiles {:debug {:debug true
+                    :injections [(prn (into {} (System/getProperties)))]
+                   }
+            ;; activated automatically during uberjar
+            :uberjar {:aot :all}
+ }
 )
